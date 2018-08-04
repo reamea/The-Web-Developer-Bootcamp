6178d6543198310f3a90e19300484b30e3d52f4d
Reamea-Mac <cheyreamea7@gmail.com>
2018-08-04 17:11:13 +0700

    Java Script
    JS Stalker Excercise

--- Java Script/script.js
+++ Java Script/script.js
@@ -1,1 +1,3 @@
-alert ("Hello from the JS File!");
+var userName = prompt ("What is your name?");
+alert ("Nice to meet you, " + userName);
+console.log ("Also greate to meet you, " + userName);
---
+++ Java Script/stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name");
+var lastName = prompt("What is your last name");
+var fullName = firstName + " " + lastName;
+var age = prompt("what is your age");
+console.log("Your full name is " + fullName);
+console.log("You are " + age + " years old");
